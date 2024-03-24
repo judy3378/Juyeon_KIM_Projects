e863c94e6e258fd8d7f12bb4ac5a4c7c817afaa2
Juyeon <81371179+judy3378@users.noreply.github.com>
2024-03-24 13:38:26 +0100

    Update Corridor_robot.py

--- project_ROS/src/Corridor_robot.py
+++ project_ROS/src/Corridor_robot.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""works"""
+# Author: Juyeon Kim
+"""tested"""
 import rospy
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
